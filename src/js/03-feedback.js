9ca977e8804ebabbfcd79489c6bba8fc5e57b2e2
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-16 18:04:30 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,15 @@
 var throttle = require('lodash.throttle');
 
+// ================== take refs  ==================
 const refs = {
     form: document.querySelector('.feedback-form'),
     email: document.querySelector('input'), 
     message: document.querySelector('textarea'), 
 };
+
+// ================== make storage key  ==================
+
+const STORAGE_KEY = "feedback-form-state"
 
 // ================== check after reload  ==================
 
@@ -17,22 +22,21 @@ refs.form.addEventListener('submit', onSubmit);
 
 
 // ================== data storage ==================
-
 // const savedData = {
 //     // email: "",
 //     // message: "",
 //     };
 
 // ==================  save input ==================
+
 function onFormInputs(e) {
-    let savedData = localStorage.getItem("feedback-form-state")
+    let savedData = localStorage.getItem(STORAGE_KEY)
     savedData = savedData ? JSON.parse(savedData) : {};
 
     savedData[e.target.name] = e.target.value;
     
     console.log(savedData);
-    localStorage.setItem("feedback-form-state", JSON.stringify(savedData))
-    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData))
 };
 
 // ==================  submit actions ==================
@@ -46,42 +50,40 @@ function onSubmit(e) {
         console.log(`email: ${formEmail}`);
         console.log(`message: ${formTextarea}`);
 
-        localStorage.removeItem("feedback-form-state")
+        localStorage.removeItem(STORAGE_KEY)
         refs.form.reset()
     } else {
         alert("Заполните все поля");
     };
-
-    
-    
 };
 
 // ================== take data after reload ==================
 function renewFormInputs() {
-    let savedData = localStorage.getItem("feedback-form-state")
+    let savedData = localStorage.getItem(STORAGE_KEY)
     
     if (savedData) {
         savedData = JSON.parse(savedData);
-        Object.entries(savedData).forEach(([name, value]) => {
-            refs.form.elements[name].value = value;
-        })
-    }
+        refs.email.value = savedData.email;
+        refs.message.value = savedData.message
+
+        //make it with object metod
+        // Object.entries(savedData).forEach(([name, value]) => {
+        //     refs.form.elements[name].value = value;
+        // })
+    };
     
 };
 
-
-        // if (formDataCome.email) {
+// if (formDataCome.email) {
         //     refs.email.value = formDataCome.email
         // } else {
         //     refs.email.value = ''
         // };
-
         // if (formDataCome.message) {
         //     refs.message.value = formDataCome.message
         // } else {
         //     refs.message.value = ''
         // }
-
         // console.log(formDataCome);
         // refs.email.value = formData.email
         // refs.message.value = formData.message
