0eecded8502f6d0e777a55c95c7db9561dfc19db
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-16 13:35:22 +0200

    p

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,50 +7,75 @@ const refs = {
 };
 
 
-refs.form.addEventListener('submit', onSubmit);
-refs.form.addEventListener('input', throttle(onFormInputs, 1000));
-// refs.email.addEventListener('input', onEmailInput);
-// refs.message.addEventListener('input', onMessageInput);
-
 renewFormInputs()
 
-const formData = {};
-// localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+refs.form.addEventListener('input', throttle(onFormInputs, 1000));
+refs.form.addEventListener('submit', onSubmit);
 
 
+// ================== data storage ==================
+
+const formData = {
+    // email: "",
+    // message: "",
+    };
+
+// ==================  save input ==================
 function onFormInputs(e) {
     
-    formData[e.target.name] = e.target.value
+    console.log(e);
+    // formData[e.target.name] = '';
+    
+    formData[e.target.name] = e.target.value;
     
     console.log(formData);
-    const dataFormJSON = JSON.stringify(formData)
-    console.log(dataFormJSON);
     localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+    
 };
 
-
+// ==================  submit actions ==================
 function onSubmit(e) {
     e.preventDefault();
     
     const formEmail = e.target.elements.email.value;
-    const formTextarea = e.target.elements.message.value
+    const formTextarea = e.target.elements.message.value;
 
-    console.log(`email: ${formEmail}`);
-    console.log(`message: ${formTextarea}`);
+    if (formEmail && formTextarea) {
+        console.log(`email: ${formEmail}`);
+        console.log(`message: ${formTextarea}`);
+
+        localStorage.removeItem("feedback-form-state")
+        refs.form.reset()
+    } else {
+        alert("Заполните все поля");
+    };
+
     
-    refs.form.reset()
-    localStorage.removeItem("feedback-form-state")
+    
 };
 
+// ================== take data after reloud ==================
 function renewFormInputs() {
     const savedData = localStorage.getItem("feedback-form-state")
+
     if (savedData) {
-        const formData = JSON.parse(savedData)
-        console.log(formData);
+        const formDataCome = JSON.parse(savedData)
 
+        if (formDataCome.email) {
+            refs.email.value = formDataCome.email
+        } else {
+            refs.email.value = ''
+        };
 
-        refs.email.value = formData.email
-        refs.message.value = formData.message
+        if (formDataCome.message) {
+            refs.message.value = formDataCome.message
+        } else {
+            refs.message.value = ''
+        }
+
+        console.log(formDataCome);
+        // refs.email.value = formData.email
+        // refs.message.value = formData.message
     }
 };
 
